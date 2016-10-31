ac1726fa9519493c8436cda80de8bef116fd8cca
dekespo <dekespo@gmail.com>
2016-10-31 20:03:51 +0100

    added matplotlib to the frame

--- gui/plot.py
+++ gui/plot.py
@@ -1,11 +1,21 @@
 import tkinter as tk
 import constants as CONST
 
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+import matplotlib.pyplot as plt
+import numpy as np
+
 class plotGUI(tk.Frame):
     def __init__(self, parent):
         canvas = tk.Canvas(parent, bg = "blue", width = CONST.plotWindow.width, height = CONST.plotWindow.height)
-        #text = tk.StringVar()
-        #label = tk.Label(canvas, textvariable = text)
-        #text.set("Plot Part")
-        #label.pack(anchor = tk.CENTER)
-        canvas.grid(row = 0, column = 1, sticky = "nesw")
+        fig = plt.Figure()
+        plotCanvas = FigureCanvasTkAgg(fig, master = canvas)
+        #toolbar = NavigationToolbar2TkAgg(plotCanvas, canvas) # weird error, assumes canvas is pack?
+        plotCanvas.get_tk_widget().grid(row = 0, column = 0)
+        #toolbar.grid(row = 1, column = 0)
+        canvas.grid(row = 0, column = 1)
+
+        x = np.arange(0, 2 * np.pi, 0.01)
+        ax = fig.add_subplot(111)
+        line, = ax.plot(x, np.sin(x))
+
