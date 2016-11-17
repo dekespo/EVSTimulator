9e2353120ac239676248a8de5a4ba0006b657a36
Marcus Näslund <naslundx@gmail.com>
2016-11-17 19:58:15 +0100

    PEP8 compliancy

--- constants.py
+++ constants.py
@@ -2,11 +2,13 @@ class mainWindow:
     width, height = 800, 600
     title = "EVSTimulator"
 
+
 class simulationWindow:
-    width, height = mainWindow.width * 2 // 3, mainWindow.height * 4 // 5 
+    width, height = mainWindow.width * 2 // 3, mainWindow.height * 4 // 5
     rowOrder, colOrder = 0, 0
     background = "black"
     sticky = "nsew"
+
 
 class buttonsWindow:
     width, height = mainWindow.width * 4 // 5, mainWindow.height * 1 // 5
@@ -14,17 +16,20 @@ class buttonsWindow:
     background = "yellow"
     sticky = "nsew"
 
+
 class textWindow:
     width, height = mainWindow.width * 1 // 5, mainWindow.height * 1 // 5
     rowOrder, colOrder = 1, 1
     background = "purple"
     sticky = "nsew"
 
+
 class plotWindow:
     width, height = mainWindow.width * 1 // 3, mainWindow.height * 4 // 5
     rowOrder, colOrder = 0, 1
     background = "blue"
     sticky = "nsew"
+
 
 class bubbles:
     numberOfCircles = 25
--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
 import math
 
+
 def euclidDistance(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
-    return math.sqrt( (x2 - x1) ** 2 + (y2 - y1) ** 2)
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
 
 def circlesIsOverlapCheck(c1, c2):
     r1, r2 = c1.radius, c2.radius
